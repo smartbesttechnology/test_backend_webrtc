02f62180a38e2e6df5b927ab75a4c363a95f4317
emmaro <emmaro@gmail.com>
2024-07-06 23:55:28 +0100

    test

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const { userSignupController } = require("./controller");
 const router = require("express").Router();
 
 router.get("/signup", (req, res) => {
-    res.send('hello man')
+    res.send('hello man');
 });
 
 module.exports = router
